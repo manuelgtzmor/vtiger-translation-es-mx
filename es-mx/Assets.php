cc9fc235faf625c24b2e3a5763f3a05ae9f9148d
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-11 23:42:31 -0600

    Agregar archivo de traducción mejorado Assets.php - Add improved Assets.php translation file

---
+++ es-mx/Assets.php
@@ -0,0 +1,36 @@
+<?php
+/************************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	// Cadenas Básicas
+	'Assets'        			   => 'Activos'                      ,
+	'SINGLE_Assets' 			   => 'Activo'                       ,
+	'LBL_ADD_RECORD' 			   => 'Agregar Activo'               ,
+	'LBL_RECORDS_LIST' 			   => 'Lista de Activos'             ,
+
+	// Bloques
+	'LBL_ASSET_INFORMATION'        => 'Detalles del Activo'          ,
+
+	//Etiquetas de campos
+	'Asset No'                     => 'Número de Activo'             ,
+	'Serial Number'                => 'Número de serie'              ,
+	'Date Sold'                    => 'Fecha de Venta'               , 
+	'Date in Service'              => 'Fecha de Instalación'         , 
+	'Tag Number'                   => 'Número de Etiqueta'           , 
+	'Invoice Name'                 => 'Factura Relacionada'          ,
+	'Shipping Method'              => 'Forma de Envío'               , 
+	'Shipping Tracking Number'     => 'Número de Rastreo del Envío'  , 
+	'Asset Name'                   => 'Nombre del Activo'            , 
+	'Customer Name'                => 'Nombre del Cliente'           , 
+	'Notes'                        => 'Notas'                        , 
+
+	//Valores de Listas Desplegables
+	'In Service'                   => 'En Servicio'                  , 
+	'Out-of-service'               => 'Fuera de servicio'            , 
+);
