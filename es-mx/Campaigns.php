1fe2a19602ee3da1f40c7d3d3f31bb127d2b9c3d
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-12 00:10:33 -0600

    Agregar archivo de traducción mejorado Campaigns.php  /  Add improved Campaigns.php translation file

---
+++ es-mx/Campaigns.php
@@ -0,0 +1,56 @@
+<?php
+/************************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'Active'					   => 'Activa'
+	'Actual Cost'                  => 'Costo Real'                 				,
+	'Actual Response Count'        => 'Cantidad de Respuestas Real'             ,
+	'Actual ROI'                   => 'Retorno de Inversión Real'       		,
+	'Actual Sales Count'           => 'Cantidad de Ventas Real'                 ,
+	'Advertisement'                => 'Publicidad'                  			,
+	'Average'                      => 'Regular'                      			,
+	'Banner Ads'                   => 'Banners'                     			,
+	'Budget Cost'                  => 'Costo Estimado'              			,
+	'Campaign Name'                => 'Nombre de Campaña'          				,
+	'Campaign No'                  => 'Número de Campaña'              			,
+	'Campaign Status'              => 'EStatus de la Campaña'      				,
+	'Campaign Type'                => 'Tipo de Campaña'            				,
+	'Campaigns'                    => 'Campañas'                   				,
+	'Cancelled'                    => 'Cancelada'                   			,
+	'Completed'                    => 'Completada'                    			,
+	'Contacted - Never Contact Again' => 'Contactado - No volver a contactar'	, 
+	'Contacted - Successful'       => 'Contactado - Éxito'         				, 
+	'Contacted - Unsuccessful'     => 'Contactado - Fracaso'        			,
+	'Direct Mail'                  => 'Correo Directo'	                        ,
+	'Excellent'                    => 'Excelente'                   			,
+	'Expected Response'            => 'Respuesta Esperada'          			,
+	'Expected Response Count'      => 'Cantidad de Respuestas Estimada'		    ,
+	'Expected Revenue'             => 'Ingreso Esperado'          				,
+	'Expected ROI'                 => 'Retorno de Inversión Estimada'           ,
+	'Expected Sales Count'         => 'Cantidad de Ventas Estimada'		        ,
+	'Good'                         => 'Buena'                       			,
+	'Inactive'                     => 'Inactiva'                    			,
+	'LBL_ADD_RECORD'               => 'Agregar Campaña'            				,
+	'LBL_CAMPAIGN_INFORMATION'     => 'Detalles de la Campaña'    				,
+	'LBL_EXPECTATIONS_AND_ACTUALS' => 'Expectativas y Resultados Reales'        ,
+	'LBL_RECORDS_LIST'             => 'Lista de Campañas'          				,
+	'--None--'                     => '--Ninguno--'                 			,
+	'Num Sent'                     => 'Número de Envíos'          				,
+	'Others'                       => 'Otros'                       			,
+	'Planning'                     => 'Planeada'                    			,
+	'Poor'                         => 'Pobre'                       			,
+	'Product'                      => 'Producto'                    			,
+	'Referral Program'             => 'Programa de Referidos'       			,
+	'SINGLE_Campaigns'             => 'Campaña'                    				,
+	'Sponsor'                      => 'Patrocinador'                			,
+	'Target Audience'              => 'Público Objetivo'           				,
+	'TargetSize'                   => 'Tamaño de Público Objetivo'       		,
+	'Telemarketing'                => 'Telemercadotecnia'              			,
+	'Webinar'                      => 'Webinario'                     			,
+);
