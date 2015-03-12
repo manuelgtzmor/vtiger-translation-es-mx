3aeabee3c4441251aa37f341bab18fd6dcbef2d4
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-12 00:42:36 -0600

    Agregar archivo de traducción mejorado Contacts.php / Add improved Contacts.php translation file

---
+++ es-mx/Contacts.php
@@ -0,0 +1,58 @@
+<?php
+/************************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'Assistant'                    => 'Asistente'                  		, 
+	'Assistant Phone'              => 'Teléfono del Asistente'  		,
+	'Birthdate'                    => 'Fecha de Nacimiento'         	,
+	'Contact Id'				   => 'Número de Contacto'				,
+	'Contact Image'                => 'Imagen del Contacto'         	,
+	'Contacts'                     => 'Contactos'                   	,
+	'Department'                   => 'Departamento'                	,
+	'Dr.'                          => 'Dr.'                         	,
+	'Do Not Call'                  => 'No Llamar por Teléfono'     		,
+	'Email' 					   => 'Correo'							,
+	'Home Phone'                   => 'Teléfono Particular'             ,
+	'LBL_ADD_RECORD'               => 'Agregar Contacto'            	, 
+	'LBL_CONTACT_INFORMATION'      => 'Datos del Contacto'            	, 
+	'LBL_CUSTOMER_PORTAL_INFORMATION' => 'Detalles - Portal del Cliente', 
+	'LBL_COPY_MAILING_ADDRESS'     => 'Copiar Dirección de Facturación' ,
+	'LBL_COPY_OTHER_ADDRESS'       => 'Copiar Dirección de Envío'   	, 
+	'LBL_IMAGE_INFORMATION'        => 'Foto del Contacto'				, 
+	'LBL_RECORDS_LIST'             => 'Lista de Contactos'          	,
+	'Mailing City'                 => 'Deleg./Mpio. (Facturación)'      ,
+	'Mailing Country'              => 'País (Facturación)'              ,
+	'Mailing Po Box'               => 'Colonia (Facturación)'           ,
+	'Mailing State'                => 'Estado (Facturación)'            ,
+	'Mailing Street'               => 'Calle y Número (Facturación)'    , 
+	'Mailing Zip'                  => 'Código Postal (Facturación)'     ,
+	'Mr.'                          => 'Sr.'                         	, 
+	'Ms.'                          => 'Sra.'                        	, 
+	'Mrs.'                         => 'Srta.'                       	,
+	'Office Phone'                 => 'Teléfono de Oficina'             ,
+	'Other City'                   => 'Deleg./Mpio. (Envío)'       		,
+	'Other Country'                => 'País (Envío)'              		,
+	'Other Po Box'                 => 'Colonia (Envío)'            		,
+	'Other State'                  => 'Estado (Envío)'             		, 
+	'Other Street'                 => 'Calle y Número (Envío)'         	, 
+	'Other Zip'                    => 'Código Postal (Envío)'     		,
+	'Prof.'                        => 'Prof.'                       	,
+	'Reference'                    => 'Usar como Referencia'           	,
+	'Reports To'                   => 'Reporta a'                   	,
+	'Secondary Email' 			   => 'Correo Secundario'				,
+	'SINGLE_Contacts'              => 'Contacto'                    	,
+	'Support End Date'             => 'Fecha de Fin de Soporte'			,
+	'Support Start Date'           => 'Fecha de Inicio de Soporte'      ,
+	'Title'                        => 'Cargo'                       	,
+	'Portal User'                  => 'Usuario del Portal'           	,
+	'User List'                    => 'Lista de Usuarios'           	,   
+);
+
+$jsLanguageStrings = array(
+);
