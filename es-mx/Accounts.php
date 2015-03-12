932fd19304c6f13b494e5b1eb9075025a11b3ea0
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-11 23:41:41 -0600

    Add improved Accounts.php translation file - Agregar archivo de traducción mejorado Accounts.php

---
+++ es-mx/Accounts.php
@@ -0,0 +1,50 @@
+<?php
+/************************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'Account Name'                 => 'Nombre de la Cuenta'          ,
+	'Account No'                   => 'Número de Cuenta'             ,
+	'Accounts'                     => 'Cuentas'                      ,
+	'Analyst'                      => 'Analista'                     ,
+	'Competitor'                   => 'Competencia'                  , 
+	'Customer'                     => 'Cliente'                      ,
+	'Email' 					   => 'Correo Principal'			 ,
+	'Employees'                    => 'Empleados'                    ,
+	'industry'                     => 'Industria'                    ,
+	'Integrator'                   => 'Integrador'                   , 
+	'Investor'                     => 'Inversionista'                ,
+	'LBL_ADD_RECORD'               => 'Agregar cuenta'               ,	
+	'LBL_ACCOUNT_INFORMATION'      => 'Detalles de la Cuenta'        ,
+	'LBL_COPY_SHIPPING_ADDRESS'    => 'Copiar Dirección de Envío'       ,
+	'LBL_COPY_BILLING_ADDRESS'     => 'Copiar Dirección de Facturación' ,
+	'LBL_DUPLICATES_EXIST'         => 'Este nombre de cuenta ya existe' ,
+	'LBL_END_DATE'                 => 'Fecha de Vencimiento'         ,
+	'LBL_SHOW_ACCOUNT_HIERARCHY'   => 'Jerarquía de Cuentas'         ,
+	'LBL_START_DATE'               => 'Fecha de Inicio'              ,
+	'LBL_RECORDS_LIST'             => 'Lista de Cuentas'             ,
+	'Member Of'                    => 'Miembro de'                   ,
+	'Other Email'                  => 'Correo Secundario'            ,
+	'Other Phone' 				   => 'Teléfono Secundario'			 ,
+	'Ownership'                    => 'Propietario'                  ,
+	'Phone' 					   => 'Teléfono Principal'	 		 ,
+	'Press'                        => 'Prensa'                       , 
+	'Prospect'                     => 'Prospecto'                    ,
+    'Prospect Accounts'            => 'Cuentas Prospecto'            ,
+	'Reseller'                     => 'Revendedor'                   ,
+	'SIC Code'                     => 'RFC'                          ,
+	'SINGLE_Accounts'              => 'Cuenta'                       , 
+	'Ticker Symbol'                => 'Símbolo de Bolsa'             ,
+	'Website'                      => 'Sitio Web'                    ,	
+);
+$jsLanguageStrings = array(
+	'JS_DUPLICATE_CREATION_CONFIRMATION' => 'Este nombre de cuenta ya existe. ¿Está seguro que quiere crear un registro duplicado?',
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => '¿Está seguro que quiere eliminar este registro?', 
+	'LBL_DELETE_CONFIRMATION'      => 'Eliminando esta cuenta, se eliminarán también todas las oportunidades y cotizaciones relacionadas. ¿Está seguro de que quiere eliminar esta Cuenta?', 
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Eliminando esta cuenta, se eliminarán también todas las oportunidades y cotizaciones relacionadas. ¿Está seguro de que quiere eliminar estas Cuentas?', 
+);
