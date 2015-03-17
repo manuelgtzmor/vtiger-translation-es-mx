b977dc61d29420db159a11b2ff8072d0ce04ae28
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-16 19:38:57 -0600

    Mejorar traducciones del archivo Invoice.php / Improve translations on file Invoice.php

--- es-mx/Invoice.php
+++ es-mx/Invoice.php
@@ -6,31 +6,39 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
-	'SINGLE_Invoice'               => 'Factura'                     , 
-	'LBL_EXPORT_TO_PDF'            => 'Exportar a PDF'              , 
-	'LBL_SEND_MAIL_PDF'            => 'Enviar correo con PDF'         , 
-	'LBL_ADD_RECORD'               => 'Agregar Factura'             , 
-	'LBL_RECORDS_LIST'             => 'Lista de Facturas'           , 
-	'LBL_INVOICE_INFORMATION'      => 'Información de la Facturación', 
-	'Sales Order'                  => 'Pedido'                      , 
-	'Customer No'                  => 'Código de cliente'          , 
-	'Invoice Date'                 => 'Fecha de Factura'            , 
-	'Purchase Order'               => 'Orden de Compra'             , 
-	'Sales Commission'             => 'Comisión sobre Ventas'      , 
-	'Invoice No'                   => 'Nº Factura'                 , 
-	'LBL_RECEIVED'                 => 'Recibido'                    , 
-	'LBL_BALANCE'                  => 'Saldo'                     , 
-	'Sent'                         => 'Enviada'                     , 
-	'Credit Invoice'               => 'a Crédito'                  , 
-	'Paid'                         => 'Pagada'                      , 
-	'AutoCreated'                  => 'Automática'                 , 
-	'Cancel'                       => 'Cancelar'                    , 
-	'LBL_NOT_A_BUNDLE' 			   => 'No es Kit de productos',
+	// Aunque la traducción de la palabra "invoice" al español es "factura", en el caso de México
+	// una factura debe ser un documento con sello del SAT y que sea fiscalmente válido. Por lo
+	// tanto, puede ser más conveniente traducir "invoice" como "nota de venta", pues describe
+	// mejor el documento en términos fiscales.
+	'SINGLE_Invoice'               => 'Nota de Venta', 
+	'LBL_EXPORT_TO_PDF'            => 'Exportar a PDF', 
+	'LBL_SEND_MAIL_PDF'            => 'Enviar Correo con PDF', 
+	'LBL_ADD_RECORD'               => 'Agregar Nota de Venta', 
+	'LBL_RECORDS_LIST'             => 'Lista de Notas de Venta', 
+	'LBL_INVOICE_INFORMATION'      => 'Detalles de la Nota de Venta', 
+	'Sales Order'                  => 'Pedido', 
+	'Customer No'                  => 'Número de Cliente', 
+	'Invoice Date'                 => 'Fecha de la Nota de Venta', 
+	'Purchase Order'               => 'Orden de Compra', 
+	'Sales Commission'             => 'Comisión sobre Ventas', 
+	'Invoice No'                   => 'Número de Nota de Venta', 
+	'LBL_RECEIVED'                 => 'Recibido', 
+	'LBL_BALANCE'                  => 'Saldo', 
+	'Sent'                         => 'Enviada', 
+	'Credit Invoice'               => 'a Crédito', 
+	'Paid'                         => 'Pagada', 
+	'AutoCreated'                  => 'Creada Automáticamente', 
+	'Cancel'                       => 'Cancelar', 
+	'LBL_NOT_A_BUNDLE' 			   => 'No es Kit de Productos',
 	'LBL_SUB_PRODUCTS'		       => 'Sub Productos',
 	'LBL_ACTION'				   => 'Acción',
+	'LBL_IS_DELETED_FROM_THE_SYSTEM_PLEASE_REMOVE_OR_REPLACE_THIS_ITEM'          => 'está eliminado del sistema. Por favor quite o reemplace este artículo',
+	'LBL_THIS_LINE_ITEM_IS_DELETED_FROM_THE_SYSTEM_PLEASE_REMOVE_THIS_LINE_ITEM' => 'Este artículo de línea está eliminado del sistema, por favor quite este artículo.',
+	'LBL_THIS'                     => 'Este',
+);
+
+$jsLanguageStrings = array(
+	'JS_PLEASE_REMOVE_LINE_ITEM_THAT_IS_DELETED' => 'Por favor quite el artículo de línea que está eliminado',
 );
