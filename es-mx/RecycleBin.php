944a9e9cb9141fdd25ea068da72e44e96f88736b
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-19 00:35:05 -0600

    Mejorar traducciones del archivo RecycleBin.php / Improve translations on file RecycleBin.php

--- es-mx/RecycleBin.php
+++ es-mx/RecycleBin.php
@@ -6,20 +6,20 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
-	'Recycle Bin'                   => 'Papelera de reciclaje'                 , 
-	'LBL_SELECT_MODULE'            => 'Seleccionar módulo'               , 
-	'LBL_EMPTY_RECYCLEBIN'         => 'Vaciar Papelera'           , 
-	'LBL_RESTORE'                  => 'Restaurar'                     , 
+	'Recycle Bin'                  => 'Papelera de Reciclaje', 
+	'LBL_SELECT_MODULE'            => 'Seleccionar módulo', 
+	'LBL_EMPTY_RECYCLEBIN'         => 'Vaciar Papelera', 
+	'LBL_RESTORE'                  => 'Restaurar', 
 	'LBL_NO_PERMITTED_MODULES'     => 'No hay módulos disponibles', 
-	'LBL_RECORDS_LIST'             => 'Lista de registros de papelera'            , 
+	'LBL_RECORDS_LIST'             => 'Lista de Registros en la Papelera', 
 	'LBL_NO_RECORDS_FOUND'         => 'No se encontraron registros para restaurar en el módulo', 
 );
+
 $jsLanguageStrings = array(
-	'JS_MSG_EMPTY_RB_CONFIRMATION' => 'Estas seguro de quieres eliminar PERMANENTEMENTE todos los registros borrados de tu base de datos?', 
-	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => 'Estas seguro que quieres restaurar los registros seleccionados?', 
+	'JS_MSG_EMPTY_RB_CONFIRMATION' => '¿Está seguro que quiere eliminar permanentemente todos los registros borrados de su base de datos?', 
+	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => '¿Está seguro que quiere restaurar los registros seleccionados?',
+	'JS_RESTORING_RECORDS' => 'Restaurando registros',
 );
+?>
