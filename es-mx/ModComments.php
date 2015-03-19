90627e2d7618673ea37197cfae1066509f1f1fce
Manuel Gutierrez Morin <manuel.gtzmor@gmail.com>
2015-03-18 19:37:41 -0600

    Mejorar traducciones del archivo ModComments.php / Improve translations on file ModComments.php

--- es-mx/ModComments.php
+++ es-mx/ModComments.php
@@ -6,16 +6,13 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
 	'SINGLE_ModComments'           => 'Comentario'                  , 
-	'LBL_RECORDS_LIST'             => 'Lista de comentarios'        , 
-	'LBL_MODCOMMENTS_INFORMATION'  => 'Información de Comentario'  , 
+	'LBL_RECORDS_LIST'             => 'Lista de Comentarios'        , 
+	'LBL_MODCOMMENTS_INFORMATION'  => 'Información del Comentario'  , 
 	'LBL_OTHER_INFORMATION'        => 'Información Adicional'      , 
-	'LBL_ADDING_COMMENT'           => 'Agregar Comentario'          , 
+	'LBL_ADDING_COMMENT'           => 'Agregando Comentario'          , 
 	'LBL_WRITE_YOUR_COMMENT_HERE'  => 'Introduzca comentarios aquí', 
 	'Comment'                      => 'Comentario'                  , 
 	'Creator'                      => 'Creador'                     , 
