e39b4139d299a5c435b0f28d19a9241bcb6199ed
Manuel Gutiérrez Morín <manuel.gtzmor@gmail.com>
2015-03-19 00:34:32 -0600

    Mejorar traducciones del archivo Quotes.php / Improve translations on file Quotes.php

--- es-mx/Quotes.php
+++ es-mx/Quotes.php
@@ -6,9 +6,6 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
 	'SINGLE_Quotes'                => 'Cotización'                 , 
@@ -16,10 +13,10 @@ $languageStrings = array(
 	'LBL_SEND_MAIL_PDF'            => 'Enviar correo con PDF'         , 
 	'LBL_ADD_RECORD'               => 'Agregar Cotización'         , 
 	'LBL_RECORDS_LIST'             => 'Lista de Cotizaciones'       , 
-	'LBL_QUOTE_INFORMATION'        => 'Información de la Cotización', 
-	'Quote No'                     => 'No Cotización'              , 
-	'Quote Stage'                  => 'Estado de la Cotización'    , 
-	'Valid Till'                   => 'Válida Hasta'               , 
+	'LBL_QUOTE_INFORMATION'        => 'Detalles de la Cotización', 
+	'Quote No'                     => 'Número de Cotización', 
+	'Quote Stage'                  => 'Estado de la Cotización', 
+	'Valid Till'                   => 'Válida Hasta', 
 	'Inventory Manager'            => 'Encargado del Inventario'    , 
 	'Accepted'                     => 'Aceptado'                    , 
 	'Rejected'                     => 'Rechazado'                   , 
