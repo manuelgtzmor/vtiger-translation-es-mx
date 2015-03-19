02a8d43ccd236d5da3d5ef4b54ebafdabbc8a695
Manuel Gutierrez Morin <manuel.gtzmor@gmail.com>
2015-03-18 19:40:57 -0600

    Mejorar traducciones del archivo PurchaseOrder.php / Improve translations on file PurchaseOrder.php

--- es-mx/PurchaseOrder.php
+++ es-mx/PurchaseOrder.php
@@ -6,27 +6,32 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
+	//DetailView Actions
 	'SINGLE_PurchaseOrder'         => 'Orden de Compra'             , 
 	'LBL_EXPORT_TO_PDF'            => 'Exportar a PDF'              , 
-	'LBL_SEND_MAIL_PDF'            => 'Enviar correo con PDF'         , 
+	'LBL_SEND_MAIL_PDF'            => 'Enviar correo con PDF'         ,
+
+	//Frases básicas
 	'LBL_ADD_RECORD'               => 'Agregar Orden de Compra'    , 
 	'LBL_RECORDS_LIST'             => 'Lista de Ordenes de Compra' , 
 	'LBL_COPY_SHIPPING_ADDRESS'    => 'Copiar Dirección de Envío'       , 
-	'LBL_COPY_BILLING_ADDRESS'     => 'Copiar Dirección de Factura'        , 
-	'LBL_PO_INFORMATION'           => 'Información de la Orden de Compra', 
-	'PurchaseOrder No'             => 'No Orden Compra'             , 
-	'Requisition No'               => 'Referencia del pedido'       , 
-	'Tracking Number'              => 'No de Guía'          , 
-	'Sales Commission'             => 'Comisión sobre Ventas'      , 
-	'LBL_PAID'                     => 'Pagada'                        , 
-	'LBL_BALANCE'                  => 'Saldo'                     , 
-	'Received Shipment'            => 'Recibido'                    , 
-        'LBL_LIST_PRICE'               => 'Precio de lista',
-        'List Price'                   => 'Precio de lista',
-        'LBL_COPY_COMPANY_ADDRESS'=> 'Copiar Dirección de la empresa',
+	'LBL_COPY_BILLING_ADDRESS'     => 'Copiar Dirección de Facturación',
+
+	// Bloques 
+	'LBL_PO_INFORMATION'           => 'Detalles de la Orden de Compra',
+
+	// Etiquetas de Campo
+	'PurchaseOrder No'             => 'Número de Orden de Compra', 
+	'Requisition No'               => 'Número de Requisición', 
+	'Tracking Number'              => 'Número de Rastreo', 
+	'Sales Commission'             => 'Comisión sobre Ventas', 
+	'LBL_PAID'                     => 'Pagada', 
+	'LBL_BALANCE'                  => 'Saldo',
+
+	'Received Shipment'            => 'Mercancía Recibida', 
+        'LBL_LIST_PRICE'               => 'Precio de Lista',
+        'List Price'                   => 'Precio de Lista',
+    'LBL_COPY_COMPANY_ADDRESS'=> 'Copiar Dirección de la empresa',
 );
