68fe50dd95c0e5b92fbc4a6c7e48b41fffd541f2
Manuel Gutierrez Morin <manuel.gtzmor@gmail.com>
2015-03-29 23:16:39 -0600

    Mejorar traducciones del archivo Groups.php / Improve translations on file Groups.php

--- es-mx/Settings/Groups.php
+++ es-mx/Settings/Groups.php
@@ -6,22 +6,28 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *  Author       : Francisco Hernandez Odin Consultores S de RL de CV
- *  Author       : www.odin.mx
- *  Author       : Proudly, the best Vtiger's Partner in Mexico
  ************************************************************************************/
 $languageStrings = array(
-	'LBL_TRANSFORM_OWNERSHIP'      => 'Transferir propietario'      , 
+	'LBL_TRANSFORM_OWNERSHIP'      => 'Transferir propiedad', 
 	'SINGLE_Groups'                => 'Grupo'                       , 
-	'LBL_TO_OTHER_GROUP'           => 'A otro grupo '               , 
+	'LBL_TO_OTHER_GROUP'           => 'A Otro Grupo ', 
 	'LBL_ADD_RECORD'               => 'Agregar Grupo'                   , 
 	'LBL_GROUP_NAME'               => 'Nombre del Grupo'                  , 
-	'LBL_GROUP_MEMBERS'            => 'Miembros del grupo'               , 
+	'LBL_GROUP_MEMBERS'            => 'Miembros del Grupo', 
 	'LBL_ADD_USERS_ROLES'          => 'Agregar Usuarios, Roles...'         , 
 	'LBL_ROLEANDSUBORDINATE'       => 'Roles y Subordinados'       , 
-	'RoleAndSubordinates'          => 'Roles y Subordinados'       , 
+	'RoleAndSubordinates'          => 'Roles y Subordinados'       ,
+	'LBL_DUPLICATES_EXIST' => 'Nombre de Grupo ya existe',
+    
+        //Member names 
+        'Groups' => 'Grupos', 
+        'Users' => 'Usuarios', 
+        'Roles' => 'Roles', 
+    
+        //Field names 
+        'Name' => 'Nombre de Grupo',
 );
 $jsLanguageStrings = array(
-	'JS_PLEASE_SELECT_ATLEAST_ONE_MEMBER_FOR_A_GROUP' => 'Selecciona al menos un miembro para el grupo', 
-	'JS_RECORD_DELETED_SUCCESSFULLY' => 'Grupo eliminado exitosamente'  , 
+	'JS_PLEASE_SELECT_ATLEAST_ONE_MEMBER_FOR_A_GROUP' => 'Por favor selecciona al menos un miembro para el grupo', 
+	'JS_RECORD_DELETED_SUCCESSFULLY' => 'Grupo eliminado exitosamente', 
 );
